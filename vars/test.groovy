c2d63872733f4e8ef4a233dbab8429017b5a526d
Tarun <tarunkumarp369@gmail.com>
2022-10-23 12:13:54 +0530

    added change in test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,57 +1,57 @@
-def call(){
-    pipeline{
-    agent {
-        label 'python3.10'
-    }
-    parameters{
-        choice(name: 'Branch_to_Build', choices: ['master','python3.10'], description: 'Selecting branch of choice')
-    }
-    triggers{
-        pollSCM('* * * * *')
-    }
-    post{
-        always{
-            echo 'Job completed'
-            mail to: 'tarunkumarpendem22@gmail.com',
+def call(url) {
+    pipeline {
+        agent {
+            label 'python3.10'
+        }
+        parameters {
+            choice(name: 'Branch_to_Build', choices: ['master', 'python3.10'], description: 'Selecting branch of choice')
+        }
+        triggers {
+            pollSCM('* * * * *')
+        }
+        post {
+            always {
+                echo 'Job completed'
+                mail to: 'tarunkumarpendem22@gmail.com',
                  subject: "Job's done",
                  body: "Job is completed for Build number: $env.BUILD_NUMBER"
-        }
-        success{
-            echo 'Job successfully completed'
-            mail to: 'tarunkumarpendem22@gmail.com',
+            }
+            success {
+                echo 'Job successfully completed'
+                mail to: 'tarunkumarpendem22@gmail.com',
                  subject: "Job's done",
                  body: "Job is successfully completed for Build number: $env.BUILD_NUMBER"
-        }
-        failure{
-            echo 'Job failed'
-            mail to: 'tarunkumarpendem22@gmail.com',
+            }
+            failure {
+                echo 'Job failed'
+                mail to: 'tarunkumarpendem22@gmail.com',
                  subject: "Job's done",
                  body: "Job is successfully failed for Build number: $env.BUILD_NUMBER"
+            }
         }
-    }
-    stages{
-        stage('clone'){
-            steps{
-                git url: 'https://github.com/tarunkumarpendem/python-webcount.git',
+        stages {
+            stage('clone') {
+                steps {
+                    git url: "${url}",
                     branch: "${params.Branch_to_Build}"
+                }
             }
-        }
-        stage('build'){
-            steps{
-                sh 'pip3 install -r requirements.txt'
-                sh 'tox'
+            stage('build') {
+                steps {
+                    sh 'pip3 install -r requirements.txt'
+                    sh 'tox'
+                }
             }
-        }
-        stage('Archiving_Artifacts'){
-            steps{
-                archiveArtifacts artifacts: '**/*.zip', followSymlinks: false
+            stage('Archiving_Artifacts') {
+                steps {
+                    archiveArtifacts artifacts: '**/*.zip', followSymlinks: false
+                }
             }
-        }
-        stage('junit-reports'){
-            steps{
-                junit '**/*.xml'
+            stage('junit-reports') {
+                steps {
+                    junit '**/*.xml'
+                }
             }
         }
     }
-}
 }
